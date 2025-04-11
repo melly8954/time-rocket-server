b3f5210c3cf147e568877f3d653e518894fc1256
haily <49448330+melly8954@users.noreply.github.com>
2025-04-11 15:29:47 +0900

    init

---
+++ src/main/java/com/melly/timerocketserver/TimeRocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.melly.timerocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeRocketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeRocketServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/melly/timerocketserver/TimeRocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melly.timerocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeRocketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
