10a75b54d599aa1611401a356e3e0dc51f22b1fa
haily <49448330+melly8954@users.noreply.github.com>
2025-05-05 23:47:43 +0900

    250505 사용자 예외 처리 적용

--- src/main/java/com/melly/timerocketserver/domain/service/UserService.java
+++ src/main/java/com/melly/timerocketserver/domain/service/UserService.java
@@ -37,7 +37,7 @@ public class UserService {
         }
         // 닉네임 중복 검사
         if(userRepository.existsByNickname(signUpRequestDto.getNickname())) {
-            throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
+            throw new DuplicateNicknameException("이미 존재하는 닉네임입니다.");
         }
         UserEntity userEntity = UserEntity.builder()
                 .email(signUpRequestDto.getEmail())
