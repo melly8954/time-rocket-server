897e5c1ec86ce24ec3b5c690105f20f47d0479c2
haily <49448330+melly8954@users.noreply.github.com>
2025-05-09 23:43:23 +0900

    250509 로켓 전송 api 및 그 외(3)
    1. 로켓 엔티티, 리포지토리, 서비스, 컨트롤러 생성
    2. 로켓 전송 api 구현
    3. 모임 엔티티, 리포지토리 생성
    4. 보관함 엔티티, 리포지토리 생성

--- src/main/java/com/melly/timerocketserver/domain/controller/RocketController.java
+++ src/main/java/com/melly/timerocketserver/domain/controller/RocketController.java
@@ -1,14 +1,26 @@
 package com.melly.timerocketserver.domain.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.melly.timerocketserver.domain.dto.request.RocketRequestDto;
+import com.melly.timerocketserver.domain.service.RocketService;
+import com.melly.timerocketserver.global.common.ResponseController;
+import com.melly.timerocketserver.global.common.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/rockets")
-public class RocketController {
-    @GetMapping("")
-    public String index() {
-        return "로켓 페이지입니다.";
+public class RocketController implements ResponseController {
+    private final RocketService rocketService;
+
+    public RocketController(RocketService rocketService) {
+        this.rocketService = rocketService;
     }
+    @PostMapping("/users/{userId}")
+    public ResponseEntity<ResponseDto> sendRocket(@PathVariable Long userId, @RequestBody RocketRequestDto rocketRequestDto) {
+        this.rocketService.sendRocket(userId, rocketRequestDto);
+        return makeResponseEntity(HttpStatus.CREATED, "로켓이 전송 되었습니다.", null);
+    }
+
+
 }
