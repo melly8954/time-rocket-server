4bb22700a4a769177238ad8046d9d7d219f0fdbe
haily <49448330+melly8954@users.noreply.github.com>
2025-05-19 05:55:54 +0900

    250519 진열장 상세 조회 api 응답 객체 필드 업데이트

--- src/main/java/com/melly/timerocketserver/domain/dto/response/DisplayDetailResponse.java
+++ src/main/java/com/melly/timerocketserver/domain/dto/response/DisplayDetailResponse.java
@@ -16,11 +16,7 @@ public class DisplayDetailResponse {
     private String designUrl;
     private String senderEmail;
     private LocalDateTime sentAt;
-
-    // 아래 두 필드는 상황에 따라 하나만 채워짐
     private String content;               // 잠금 해제 상태에서만 포함
-    private LocalDateTime lockExpiredAt; // 잠금 상태에서만 포함
-
     @JsonProperty("isLocked")  // JSON 직렬화 시 'isLocked' 으로 나오게 강제
     private boolean isLocked;
 
